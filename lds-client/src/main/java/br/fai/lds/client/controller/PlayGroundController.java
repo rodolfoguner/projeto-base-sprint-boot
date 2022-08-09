5f96972045390290bc602e33e4571da4aa07f4b4
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-08-08 21:42:47 -0300

    feat: adicao do PlayGroundController

---
+++ lds-client/src/main/java/br/fai/lds/client/controller/PlayGroundController.java
@@ -0,0 +1,13 @@
+package br.fai.lds.client.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PlayGroundController {
+
+    @GetMapping("/playground/test")
+    public String getTestPage() {
+        return "playground/test-page";
+    }
+}
