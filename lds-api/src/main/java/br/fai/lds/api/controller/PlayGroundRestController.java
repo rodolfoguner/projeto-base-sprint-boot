8ca615d5de20b7537c470c1eb2fb89c919b83b72
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-08-08 22:34:24 -0300

    feat: adicao do PlayGroundRestController

---
+++ lds-api/src/main/java/br/fai/lds/api/controller/PlayGroundRestController.java
@@ -0,0 +1,18 @@
+package br.fai.lds.api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/playground")
+@CrossOrigin(origins = "*")
+public class PlayGroundRestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> getPlayGroundData() {
+        return ResponseEntity.ok("Boa noite. =D");
+    }
+}
