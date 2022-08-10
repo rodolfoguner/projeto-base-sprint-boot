0caf34d3b1579c91ad38ed030c7ac49215199bff
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-08-09 21:20:20 -0300

    feat: adicao de BaseEntity

---
+++ lds-model/src/main/java/br/fai/models/entities/BaseEntity.java
@@ -0,0 +1,43 @@
+package br.fai.models.entities;
+
+import java.sql.Timestamp;
+
+public class BaseEntity {
+
+    private Timestamp createdAt;
+    private Timestamp lastModified;
+    private String createdBy;
+    private String lastModifiedBy;
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Timestamp lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+}
