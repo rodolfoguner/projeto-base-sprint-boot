199a837f05fb7e31d94002d3d4834d5da5b78a20
Rodolfo Mamud <rodolfo@pdsolucoes.com.br>
2022-08-09 21:21:54 -0300

    feat: adicao do user

---
+++ lds-model/src/main/java/br/fai/models/entities/UserModel.java
@@ -0,0 +1,78 @@
+package br.fai.models.entities;
+
+import br.fai.models.enums.UserType;
+
+public class UserModel extends BaseEntity {
+    private int id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private UserType type;
+    private Boolean isActive;
+    private byte[] avatar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public UserType getType() {
+        return type;
+    }
+
+    public void setType(UserType type) {
+        this.type = type;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public byte[] getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(byte[] avatar) {
+        this.avatar = avatar;
+    }
+}
