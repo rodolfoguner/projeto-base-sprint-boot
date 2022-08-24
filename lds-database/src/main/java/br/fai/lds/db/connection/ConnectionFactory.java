b1d26f6e7f85210c82d2b4c56494643c82f9ed6c
enthony <rodolfo@pdsolucoes.com.br>
2022-08-23 21:16:38 -0300

    feat: adicao da conexao do banco de dados

--- lds-database/src/main/java/br/fai/lds/db/connection/ConnectionFactory.java
+++ lds-database/src/main/java/br/fai/lds/db/connection/ConnectionFactory.java
@@ -1,4 +1,66 @@
 package br.fai.lds.db.connection;
 
+import java.sql.*;
+
 public class ConnectionFactory {
+
+    private static Connection connection = null;
+
+    private static final String URL = "jdbc:postgresql:://localhost:3000/db_fai_lds";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    public static Connection getConnection() {
+        try {
+            if (connection != null) {
+                return connection;
+            }
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void close(PreparedStatement preparedStatement, Connection connection, ResultSet resultSet) {
+        closePreparedStatement(preparedStatement);
+        closeResultSet(resultSet);
+        closeConnection(connection);
+    }
+
+    private static void closeConnection(Connection connection) {
+        if (connection == null) {
+            return;
+        }
+
+        try {
+            connection.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void closeResultSet(ResultSet resultSet) {
+        if (resultSet == null) {
+            return;
+        }
+
+        try {
+            resultSet.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void closePreparedStatement(PreparedStatement preparedStatement) {
+        if (preparedStatement == null) {
+            return;
+        }
+
+        try {
+            preparedStatement.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
