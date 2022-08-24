bdc2a6ae628ca3102ec34c78987c7ed585c70532
enthony <rodolfo@pdsolucoes.com.br>
2022-08-23 21:16:56 -0300

    feat: adicao da comunicacao com o banco de dados

--- lds-database/src/main/java/br/fai/lds/db/dao/impl/UserDaoImpl.java
+++ lds-database/src/main/java/br/fai/lds/db/dao/impl/UserDaoImpl.java
@@ -1,8 +1,13 @@
 package br.fai.lds.db.dao.impl;
 
+import br.fai.lds.db.connection.ConnectionFactory;
 import br.fai.lds.db.dao.UserDao;
 import br.fai.models.entities.UserModel;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoImpl implements UserDao<UserModel> {
@@ -33,6 +38,42 @@ public class UserDaoImpl implements UserDao<UserModel> {
 
     @Override
     public UserModel validateUsernameAndPassword(String username, String password) {
-        return null;
+
+        UserModel user = null;
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        final String sql = "SELECT * FROM usuario u WHERE u.nome_usuario = ? AND u.senha = ?;";
+
+        connection = ConnectionFactory.getConnection();
+        try {
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, password);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (!resultSet.next()) {
+                return user;
+            }
+
+            user = new UserModel();
+            user.setId(resultSet.getInt("id"));
+            user.setUsername(resultSet.getString("nome_usuario"));
+            user.setFullName(resultSet.getString("nome_completo"));
+            user.setEmail(resultSet.getString("email"));
+            user.setActive(resultSet.getBoolean("esta_ativo"));
+            user.setLastModified(resultSet.getTimestamp("ultima_modificacao"));
+            user.setLastModifiedBy(resultSet.getString("modificado_por"));
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionFactory.close(preparedStatement, connection, resultSet);
+        }
+
+        return user;
     }
 }
