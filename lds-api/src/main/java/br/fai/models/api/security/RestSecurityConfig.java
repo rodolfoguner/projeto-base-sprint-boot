788fd32c145db9ea9068b94f6a0e2a291bd3036e
Rodolfo <rodolfo@pdsolucoes.com.br>
2022-10-31 22:38:57 -0300

    feat: adicao do filtro jwt ao projeto api

--- lds-api/src/main/java/br/fai/models/api/security/RestSecurityConfig.java
+++ lds-api/src/main/java/br/fai/models/api/security/RestSecurityConfig.java
@@ -1,9 +1,11 @@
 package br.fai.models.api.security;
 
+import br.fai.models.api.security.filter.JWTAuthorizationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @EnableWebSecurity
@@ -15,6 +17,10 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
         httpSecurity
                 .csrf()
                 .disable()
+                .addFilterAfter(
+                        new JWTAuthorizationFilter(),
+                        UsernamePasswordAuthenticationFilter.class
+                )
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated();
---
+++ lds-api/src/main/java/br/fai/models/api/security/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,94 @@
+package br.fai.models.api.security.filter;
+
+import br.fai.models.api.security.ApiSecurityConstants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        try {
+            if (!checkJWTToke(request)) {
+                SecurityContextHolder.clearContext();
+                return;
+            }
+
+            Jws<Claims> claims = validateToken(request);
+
+            if (claims == null || claims.getBody().get(ApiSecurityConstants.AUTHORITIES) == null) {
+                SecurityContextHolder.clearContext();
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied... Wasted... We got you");
+                return;
+            }
+
+            setupSpringAuthentication(claims.getBody());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Look likes we got a bug in the matrix");
+            return;
+
+        } finally {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private void setupSpringAuthentication(Claims claims) {
+
+        List<String> authorities = (List<String>) claims.get(ApiSecurityConstants.AUTHORITIES);
+
+        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                claims.getSubject(),
+                null,
+                authorities.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList())
+        );
+
+        SecurityContextHolder
+                .getContext()
+                .setAuthentication(auth);
+    }
+
+    private Jws<Claims> validateToken(HttpServletRequest request) {
+
+        String jwtToken = request
+                .getHeader(ApiSecurityConstants.HEADER)
+                .replace(ApiSecurityConstants.PREFIX, "");
+
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(ApiSecurityConstants.KEY)
+                .build()
+                .parseClaimsJws(jwtToken);
+    }
+
+    private boolean checkJWTToke(HttpServletRequest request) {
+        String authorizationHeader = request.getHeader(ApiSecurityConstants.HEADER);
+
+        if (authorizationHeader == null || authorizationHeader.startsWith(ApiSecurityConstants.PREFIX)) {
+            return false;
+        }
+
+        return true;
+    }
+}
